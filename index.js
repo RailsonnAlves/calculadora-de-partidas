ce61cb3448d604bc92ba42a9e7b14c548e9ad77d
Railson alves <raialvess3991@gmail.com>
2023-12-02 00:05:56 -0300

    primeiro verão

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
-let numVitorias = 200
-let numDerrotas = 10
+let saldoDeVitoria = calcular(100, 10)
+let elo = classicadorDeRank(saldoDeVitoria)
+
+console.log("O Heroi tem de saldo de "+ saldoDeVitoria+" está no nivel "+ elo)
+console.log(`O Heroi tem um saldo de ${saldoDeVitoria} vitorias, esta no vível ${elo}`)
+
+function calcular(vitorias, derrotas){
+    return vitorias - derrotas
+}
+
+function classicadorDeRank(saldo){
+    let rank = ""
+    if(saldo <= 10){
+        rank = "FERRO"
+    }
+    else if(saldo <= 20){
+        rank = "BRONZE"
+    }
+    else if(saldo <= 50){
+        rank = "PRATA"
+    }
+    else if(saldo <= 80){
+        rank = "OURO"
+    }
+    else if(saldo <= 90){
+        rank = "DIAMANTE"
+    }
+    else if(saldo <= 100){
+        rank = "LENDÁRIO"
+    }
+    else if(saldo > 100){
+        rank = "IMORTAL"
+    }
+
+    return rank
+}
